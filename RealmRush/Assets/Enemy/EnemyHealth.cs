b23d282a6769cf108a99bb595feaa8f65f863ebe
Büşra GÜL <gulbusra55@gmail.com>
2022-03-16 22:17:07 +0300

    Diffuculty Factor added.

--- RealmRush/Assets/Enemy/EnemyHealth.cs
+++ RealmRush/Assets/Enemy/EnemyHealth.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+[RequireComponent(typeof(Enemy))]
 public class EnemyHealth : MonoBehaviour
 {
     
     [SerializeField] int maxHitPoints = 5;
+    [Tooltip("Add amount to maxHitPoints when enemy dies.")]
+    [SerializeField] int diffucultyRamp = 1;
     int currentHitPoints = 0;
     Enemy enemy;
 
@@ -26,6 +30,7 @@ public class EnemyHealth : MonoBehaviour
         if(currentHitPoints <= 0)
         {
              gameObject.SetActive(false);
+             maxHitPoints += diffucultyRamp;
              enemy.Reward();
         }
     }
